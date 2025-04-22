b2bd4ca0b4ca9163c07eae5235b20458e454a688
Charlotte Gale <CharlotteMGale@outlook.com>
2025-04-22 13:59:00 +0100

    initial commit

---
+++ db/seed_classes.py
@@ -0,0 +1,33 @@
+import json
+import psycopg2
+from db_config import get_connection
+
+def seed_classes():
+    with open('classes.json', 'r') as file:
+        classes = json.load(file)
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    for cls in classes:
+        cur.execute(
+            """
+            INSERT INTO classes (name, description, bonuses, perks, flavor)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (name) DO NOTHING;
+            """,
+            (
+                cls.get('name'),
+                cls.get('description'),
+                json.dumps(cls.get('bonuses', {})),
+                cls.get('perks', []),
+                cls.get('flavor', '')
+            )
+        )
+    conn.commit()
+    cur.close()
+    conn.close()
+    print('Classes seeding complete.')
+
+if __name__ == '__main__':
+    seed_classes()
---
+++ db/seed_races.py
@@ -0,0 +1,33 @@
+import json
+import psycopg2
+from db_config import get_connection
+
+def seed_races():
+    with open('races.json', 'r') as file:
+        races = json.load(file)
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    for race in races:
+        cur.execute(
+            """
+            INSERT INTO races (name, description, bonuses, perks, flavor)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (name) DO NOTHING;    
+            """,
+            (
+                race.get('name'),
+                race.get('description'),
+                json.dumps(race.get('bonuses', {})),
+                race.get('perks', []),
+                race.get('flavor', '')
+            )
+        )
+    conn.commit()
+    cur.close()
+    conn.close()
+    print('Races seeding complete.')
+
+if __name__ == '__main__':
+    seed_races()

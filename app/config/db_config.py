58398c0c1d8b9a77f39eb4417c0821f84b1dc4e2
Charlotte Gale <CharlotteMGale@outlook.com>
2025-05-12 16:46:37 +0100

    install SQLAlchemy and amend db_config, secure credentials in .env file

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,11 +1,14 @@
 
-import psycopg2
+import os
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
-def get_connection():
-    return psycopg2.connect(
-        dbname='dnd_game',
-        user='postgres',
-        password='postgres',
-        host='localhost',
-        port='5432'
-    )
+load_dotenv()
+
+db = SQLAlchemy()
+
+def init_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+

ba215b99fa9d126d40ee4adfaf598046b971a546
Charlotte Gale <CharlotteMGale@outlook.com>
2025-05-12 17:20:42 +0100

    import and use Flask-Migrate in db_config

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,14 +1,17 @@
 
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 
 load_dotenv()
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def init_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
 
+    db.init_app(app)
+    migrate.init_app(app, db)
